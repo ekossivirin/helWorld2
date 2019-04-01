e5124ce96fe57589396a3f21eacbae99722e8c52
Eva Kos-Sivirin <ykoss@softserveinc.com>
2019-04-01 21:14:56 +0300

    commit

--- src/SwitchEnum.java
+++ src/SwitchEnum.java
@@ -5,36 +5,36 @@ import java.io.InputStreamReader;
 public class SwitchEnum {
     public static void main(String[] args) throws IOException {
 
-        Season season;
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input=br.readLine();
+        Season season=null;
         switch(input.toLowerCase()){
             case "january":
             case "february":
             case "december":
                 season = Season.WINTER;
-                System.out.println("It's " + season + " time!");
                 break;
             case "march":
             case "april":
             case "may":
                 season =Season.SPRING;
-                System.out.println("It's " + season + " time!");
                 break;
             case "june":
             case "july":
             case "august":
                 season=Season.SUMMER;
-                System.out.println("It's " + season + " time!");
                 break;
             case "september":
             case "october":
             case "november":
                 season=Season.AUTUMN;
-                System.out.println("It's " + season + " time!");
                 break;
                 default:
                     System.out.println("No such month!");
+        }
+        if (season!=null) {
+            System.out.println("It's " + season + " time!");
         }
 
     }
