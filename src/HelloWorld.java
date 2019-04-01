365151b05b5a2f4d73e74ac62c9fe08bc57f5ca2
Eva Kos-Sivirin <ykoss@softserveinc.com>
2019-04-01 12:00:31 +0300

    last changes

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String arg[]){
+        System.out.println("Hello World!");
+        String name="Eva";
+        System.out.println("My name is " + name);
+    }
+}
